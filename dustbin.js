ecc66ba4eccc9004bc6fec0d9c9e934fd45bb925
Tanmayy01 <74408465+Tanmayy01@users.noreply.github.com>
2021-01-20 14:56:31 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,64 @@
+class Dustbin{
+    constructor(x,y){
+        var options = {
+            isStatic: true
+        } 
+        this.x=x
+        this.y=y
+        this.width=200
+        this.height=100
+        this.thickness=10
+        this.angle=0
+
+        this.bottom=Bodies.rectangle(this.x,this.y,this.width,this.thickness,options);
+
+        this.left=Bodies.rectangle(this.x-this.width/2,this.y-this.height/2,this.thickness,this.height,options);
+        Matter.Body.setAngle(this.left,this.angle)
+
+        this.right=Bodies.rectangle(this.x+this.width/2,this.y-this.height/2,this.thickness,this.height,options);
+        Matter.Body.setAngle(this.right,-1*this.angle)
+
+        World.add(world,this.bottom)
+        World.add(world,this.left)
+        World.add(world,this.right)
+
+    }
+    
+    display(){
+    var posb=this.bottom.position;
+     var posl=this.left.position;
+     var posr=this.right.position;
+     
+     //left
+     push();
+     translate (posl.x,posl.y);
+     rectMode(CENTER);
+     angleMode(RADIANS);
+     fill(255);
+     rotate (this.angle);
+     rect(0,0,this.thickness,this.height);
+     pop ();
+
+
+     //right
+     push();
+     translate (posr.x,posr.y);
+     rectMode(CENTER);
+     angleMode(RADIANS);
+     fill(255);
+     rotate(-1*this.angle);
+     rect(0,0,this.thickness,this.height);
+     pop();
+
+     //bottom
+     push();
+     translate (posb.x,posb.y);
+     rectMode(CENTER);
+     angleMode(RADIANS);
+     fill(255);
+    
+     rect(0,0,this.width,this.thicknesst);
+     pop();
+    }
+
+}
